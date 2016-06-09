3e13a74124517f159318b38fcce5b07738388800
Dylan Harness <dharness@uwo.ca>
2016-06-09 11:04:21 -0400

    pushing

--- server.js
+++ server.js
@@ -1,53 +1,68 @@
 "use strict";
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const request = require('superagent');
+const bodyParser = require('body-parser');
 const fbAccessToken = process.env.FB_ACCESS_TOKEN;
+const engine = require('./src/engine/engine.js');
+const fs = require('fs');
+const handleMessageEvent = require('./messagingHandler.js');
+const handlePostback = require('./postbackHandler.js');
 
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send('dreams');
 });
 
+//verification
 app.get("/webhook/", (req, res) => {
-  if (req.query["hub.mode"]==="subscribe" && req.query["hub.verify_token"]==="I_AM_A_BIG_MILKSHAKE") {
-    res.send(req.query["hub.challenge"])
+  if (req.query["hub.mode"]==="subscribe" && req.query["hub.verify_token"]===process.env.VERIFY_TOKEN) {
+    res.send(req.query["hub.challenge"]);
   }
 });
 
-app.post('/webhook/', (req, res) => {
-  //all messages since the last time fb posted
-  let messaging_events = req.body.entry[0].messaging;
 
-  messaging_events.forEach( event => {
-    let sender = event.sender.id;
-    if (event.message && event.message.text) {
-      let text = event.message.text;
-      console.log(text);
-      console.log(event.message);
-      sendMessage(sender, 'hihihihihihi!!!');
-    }
-  });
-  res.sendStatus(200)
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        if (messagingEvent.optin) {
+          // receivedAuthentication(messagingEvent);
+        } else if (messagingEvent.message) {
+          handleMessageEvent(messagingEvent)
+          // receivedMessage(messagingEvent);
+        } else if (messagingEvent.delivery) {
+          // receivedDeliveryConfirmation(messagingEvent);
+        } else if (messagingEvent.postback) {
+          handlePostback(messagingEvent);
+        } else {
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
+  }
 });
 
-function sendMessage(sender, text) {
-    request.post('https://graph.facebook.com/v2.6/me/messages')
-      .query({access_token: fbAccessToken})
-      .send({
-            recipient: {id: sender},
-            message: { text },
-      })
-      .end(callback);
 
-   function callback (error, response) {
-      if (error) {
-          console.log('Error sending messages: ', error)
-      } else if (response.body.error) {
-          console.log('Error: ', response.body.error)
-      }
-  }
-}
 
 
 // listen for requests :)
