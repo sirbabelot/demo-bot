2abab6fe8cf0c59de666ff54e4a1eebfbfb7b6d8
Morgan Moskalyk <morgan.moskalyk@gmail.com>
2016-06-23 17:37:12 -0400

    updates

--- src/engine/regex.js
+++ src/engine/regex.js
@@ -7,7 +7,7 @@ module.exports = {
   num_range: /(\d-\d)|(\b(one|two|three|four|five|six|seven|eight|nine|ten|\d)( (to|or|maybe|-) )?\b(one|two|three|four|five|six|seven|eight|nine|ten|\d)?)|(\d)/ig,
   price: /\d{1,9}(?:[. ,]\d{3})*(?:[. ,]\d{2})?/ig,
   no: /(\b)n([o]|\b)([ph]|\b)(e|\b)|never|nah|I don't/ig,
-  yes: /(\b)y([eaui .]|\b)([eaphs .]|\b)([ahs .]|\b)|(sure|\bright|fo shizzle|absolutely|totally|totes)|(\bok)/ig,
+  yes: /(\b)y([eaui .]|\b)([eaphs .]|\b)([ahs .]|\b)|(sure|\bright|fo shizzle|absolutely|totally|totes|Yes)|(\bok)/ig,
   house_apt: /house|apartment|apt|entire/ig,
   individual_room: /\broom|individual/ig,
   schedule_listing: /listing|viewing|schedule/ig,
